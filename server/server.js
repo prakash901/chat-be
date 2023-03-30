f959820f7b9aba2ee8748d611235cf4f29eb6753
prakash901 <prakashtiwari901@gmail.com>
2023-03-31 01:32:01 +0545

    minor deploy fixes

--- server/server.js
+++ server/server.js
@@ -21,20 +21,20 @@ app.use('/api/notification', notificationRoutes)
 
 // -----------------------------------------------------------------------------
 
-const __dirname$ = path.resolve()
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname$, '/client/build')))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname$, 'client', 'build', 'index.html'))
+// const __dirname$ = path.resolve()
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname$, '/client/build')))
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname$, 'client', 'build', 'index.html'))
+//   })
+// } else {
+// First route
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello from  CNP CHIT-CHAT  App server',
   })
-} else {
-  // First route
-  app.get('/', (req, res) => {
-    res.status(200).json({
-      message: 'Hello from  CNP CHIT-CHAT  App server',
-    })
-  })
-}
+})
+// }
 
 // -----------------------------------------------------------------------------
 
